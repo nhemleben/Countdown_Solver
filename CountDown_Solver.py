931345b6ccc11a92bcad6049463c597fcb15ed3f
nhemleben <nhemleben@gmail.com>
2026-01-02 19:04:36 -0600

    Initial Commit

---
+++ CountDown_Solver.py
@@ -0,0 +1,152 @@
+from itertools import combinations
+import math
+import time
+import itertools
+
+
+def fix_input(nums):
+    '''Return decreasing list of nums'''
+    return sorted(nums, reverse=True)
+
+
+#Example input for function below
+    #numbers=[1,2,3,4,5,6]
+    #input_items = [(n, str(n)) for n in numbers]
+def recurse_generate(items):
+    final_candidates = []
+    for i in range(len(items)):
+        for j in range(i + 1, len(items)):
+            (a, ea), (b, eb) = items[i], items[j]
+            candidates = []
+
+            candidates.append((a + b, f"({ea} + {eb})"))
+            candidates.append((a * b, f"({ea} * {eb})"))
+
+            if a > b:
+                candidates.append((a - b, f"({ea} - {eb})"))
+            if b > a:
+                candidates.append((b - a, f"({eb} - {ea})"))
+
+            if b != 0 and a % b == 0:
+                candidates.append((a // b, f"({ea} / {eb})"))
+            if a != 0 and b % a == 0:
+                candidates.append((b // a, f"({eb} / {ea})"))
+
+            remaining_nums = [items[k] for k in range(len(items)) if k not in (i, j)]
+            for val, expr in candidates:
+                final_candidates.extend(recurse_generate(remaining_nums+ [(val, expr)]))
+            final_candidates.extend(candidates)
+    return final_candidates
+
+def mem_efficent_recurse_generate(items):
+    final_candidates = []
+    for i in range(len(items)):
+        for j in range(i + 1, len(items)):
+            a,b = items[i], items[j]
+            candidates = [ a+b, a*b, ]
+
+            if a > b:
+                candidates.append(a - b)
+            elif b > a:
+                candidates.append(b - a)
+
+            if  a % b == 0 and b != 0 :
+                candidates.append(a // b)
+            elif b % a == 0 and a !=0:
+                candidates.append(b // a)
+
+            remaining_nums = [items[k] for k in range(len(items)) if k not in (i, j)]
+            for val in candidates:
+                final_candidates.extend(mem_efficent_recurse_generate(remaining_nums+ [val]))
+            final_candidates.extend(candidates)
+    return final_candidates
+
+
+def only_valid_final_targets(nums):
+    valid_targets = [num for num in nums if num>0 and num%1 ==0]
+    return valid_targets
+
+def get_targets(items):
+    unique_targets = set()
+    for item in items:
+        if item[0] not in unique_targets:
+            unique_targets.add(item[0])
+    return unique_targets
+
+def get_mem_efficent_targets(nums):
+    unique_targets = set()
+    for num in nums:
+        if num not in unique_targets:
+            unique_targets.add(num)
+    return unique_targets
+
+
+large_numbers = [25, 50, 75, 100, 25, 50, 75, 100]
+small_numbers = [1,2,3,4,5,6,7,8,9,10, 1,2,3,4,5,6,7,8,9,10]
+
+
+def generate_countdown_sets(num_large):
+    """Generate all valid Countdown starting sets (6-num_large small + num_large large)."""
+    large_combos = itertools.combinations(large_numbers, num_large)
+    small_combos = itertools.combinations(small_numbers, 6-num_large)
+    
+    countdown_sets= [tuple(sorted(small + large)) for small, large in itertools.product(small_combos, large_combos)]
+
+    return countdown_sets
+
+
+if __name__ == "__main__":
+
+    start_time = time.time()
+
+    total_sets = generate_countdown_sets(0)
+
+    unique_total_sets = set(total_sets)
+    print(len(unique_total_sets))
+
+    targets=[]
+    count_completed = 0
+    for number_set in unique_total_sets:
+        targets.append( mem_efficent_recurse_generate(number_set) )
+        count_completed += 1
+        if count_completed %10 == 0:
+            print(count_completed)
+            end_time = time.time()
+            running_time = end_time - start_time
+            print("Running Execution time:", running_time, "seconds")
+            print('average time to complete:', running_time/count_completed)
+
+    end_time = time.time()
+
+    #print(item_list)
+    unique_targs = get_mem_efficent_targets(targets)
+    print(unique_targs)
+    print(len(unique_targs))
+    print("Total endpoints considered: " +str(len(targets)))
+    print("Execution time:", end_time - start_time, "seconds")
+
+
+
+#    total_sets = generate_countdown_sets(0)
+#    print("Total valid starting sets:", total_sets)
+#    print(len(total_sets))
+#Upper bound on number of combinations of count down sets
+#    a = []
+#    for i in range(6):
+#        a.append(  math.comb(20,6-i) * math.comb(8,i) )
+#    print(sum(a))
+
+#Actual total number of sets
+#    a = []
+#    for i in range(6):
+#        a.append( len(set( generate_countdown_sets(i))) )
+#    print(sum(a)) #19373
+
+
+
+
+
+#
+
+
+
---
+++ test_unit_tests.py
@@ -0,0 +1,25 @@
+import pytest
+import math
+from itertools import combinations
+import CountDown_Solver as CDS
+
+
+def test_two_numbers():
+    numbers = [1,2]
+    input_items = [(n, str(n)) for n in numbers]
+    targets = CDS.get_targets(CDS.recurse_generate(input_items)) 
+    assert len(targets)  ==3
+    assert max(targets)  ==3
+    assert min(targets)  ==1
+
+
+def test_num_countdown_sets():
+    no_large = CDS.generate_countdown_sets(0)
+    one_large = CDS.generate_countdown_sets(1)
+    assert len(no_large) == math.comb(20,6)
+    assert len(one_large) == math.comb(20,5) * math.comb(8,1)
+
+def test_num_countdown_sets_general():
+    for i in range(6):
+        count_down_set = CDS.generate_countdown_sets(i)
+        assert len(count_down_set) == math.comb(20,6-i) * math.comb(8,i)
